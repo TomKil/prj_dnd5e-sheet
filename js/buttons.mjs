533b106a623ee4397c88389b3d1fa832d82aa415
TomKil <TomAskKil@outlook.com>
2023-02-14 14:41:45 +0100

    tester ting og tankegang

--- js/buttons.mjs
+++ js/buttons.mjs
@@ -7,9 +7,11 @@ let langEnBtn = document.getElementById("langEnBtn");
 
     langNoBtn.addEventListener('click', function(evt) {
         console.log("Switched to norwegian");
-        //Switch to fetching language module "no.json"
+        //Fetching language module "no.json"
+        språk = "no.json"
     });   
     langEnBtn.addEventListener('click', function(evt) {
         console.log("Switched to english");
-        //Switch to fetching language module "en.json"
+        //Fetching language module "en.json"
+        språk = "en.json"
     });
