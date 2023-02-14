4f8a3f634854cab269329df82eb7b488519a12e5
TomKil <TomAskKil@outlook.com>
2023-02-14 13:21:04 +0100

    small update

--- js/buttons.mjs
+++ js/buttons.mjs
@@ -7,7 +7,9 @@ let langEnBtn = document.getElementById("langEnBtn");
 
     langNoBtn.addEventListener('click', function(evt) {
         console.log("Switched to norwegian");
+        //Switch to fetching language module "no.json"
     });   
     langEnBtn.addEventListener('click', function(evt) {
         console.log("Switched to english");
+        //Switch to fetching language module "en.json"
     });
